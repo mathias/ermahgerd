711bf97960691f405b57aaa69dcfb4522a60425d
Matt Gauger <matt.gauger@gmail.com>
2012-08-31 09:20:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ermahgerd::Application.config.secret_token = 'ec1dc8ffcc251bed44faa7aba4b7bdaaef9a6e4bc8204d80c590cac932f327aa93dd6dfd98cbccfe42eae58ea8b3c1dfa271457b06e76a4a3df081dfdadf4e39'
