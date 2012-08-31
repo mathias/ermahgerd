cdbc715e170e03e339d24eff1e6ae52099d2e476
Bendyworks Pair <dev@bendyworks.com>
2012-08-31 16:51:59 -0500

    User can edit a post.

--- spec/acceptance/crud_posts_with_html.feature
+++ spec/acceptance/crud_posts_with_html.feature
@@ -8,10 +8,17 @@ Feature: CRUD Posts with HTML
   Scenario: User can create a new blerg post
     Given I am on the new post page
     When I fill out the form and submit it
-    Then I should see the new post
+    Then I should see the post content
 
   Scenario: User can view a post on its own
-    Given some blog posts exist
+    Given a blog post exists
     And I am on the posts page
     When I go to the first post
-    Then I should see the first post content
+    Then I should see the post content
+
+  Scenario: User can edit a post
+    Given a blog post exists
+    And I am on the posts page
+    When I click to edit the first post
+    And I change the title and submit it
+    Then I should see the edited post content
--- spec/acceptance/steps/posts_steps.rb
+++ spec/acceptance/steps/posts_steps.rb
@@ -2,16 +2,24 @@ step 'some blog posts exist' do
   create_list(:post, 5)
 end
 
+step 'a blog post exists' do
+  @title = 'Moo'
+  @body = 'Woo woo'
+  create(:post, title: @title, body: @body)
+end
+
 step 'I should see the posts listed' do
-  within 'ul' do
-    page.should have_selector 'li'
+  within '#posts' do
+    page.should have_selector '.post'
   end
 end
 
 
 step 'I fill out the form and submit it' do
-  fill_in 'post_title', with: 'foo'
-  fill_in 'post_body', with: 'bar'
+  @title = 'foo'
+  @body = 'bar'
+  fill_in 'post_title', with: @title
+  fill_in 'post_body', with: @body
   click_button 'Create Post'
 end
 
@@ -20,4 +28,35 @@ step 'I should see the new post' do
     page.should have_content 'foo'
   end
   page.should have_content 'bar'
+end
+
+step 'I go to the first post' do
+  within '#posts' do
+    page.find('.post').find('a').click
+  end
+end
+
+step 'I should see the post content' do
+  within('h1') do
+    page.should have_content(@title)
+  end
+  page.should have_content(@body)
+end
+
+step 'I click to edit the first post' do
+  within '#posts' do
+    click_on 'Edit'
+  end
+end
+
+step 'I change the title and submit it' do
+  @updated_title = 'Bears are fuzzy'
+  fill_in 'post_title', with: @updated_title
+  click_button 'Update Post'
+end
+
+step 'I should see the edited post content' do
+  within 'h1' do
+    page.should have_content @updated_title
+  end
 end
